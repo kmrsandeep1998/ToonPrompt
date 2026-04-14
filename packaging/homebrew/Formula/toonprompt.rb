b6d2c1502ca39e9b4efe1ce7dd1c9dc163e61759
kmrsandeep1998 <kmrsandeep1998@gmail.com>
2026-04-14 09:10:22 +0530

    Address PR review: harden Homebrew formula and release checksum fetch

--- packaging/homebrew/Formula/toonprompt.rb
+++ packaging/homebrew/Formula/toonprompt.rb
@@ -1,4 +1,6 @@
 class Toonprompt < Formula
+  include Language::Python::Virtualenv
+
   desc "Universal prompt optimization wrapper for AI coding CLIs"
   homepage "https://github.com/kmrsandeep1998/ToonPrompt"
   url "https://github.com/kmrsandeep1998/ToonPrompt/archive/refs/tags/v0.1.0a2.tar.gz"
@@ -7,8 +9,18 @@ class Toonprompt < Formula
 
   depends_on "python@3.12"
 
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/source/p/pyyaml/pyyaml-6.0.2.tar.gz"
+    sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
+  end
+
+  resource "tomli" do
+    url "https://files.pythonhosted.org/packages/source/t/tomli/tomli-2.0.1.tar.gz"
+    sha256 "de526c12914f0c550d15924c62d72abc48d6fe7364aa87328337a31007fe8a4f"
+  end
+
   def install
-    system "pip3", "install", "--prefix=#{prefix}", "."
+    virtualenv_install_with_resources
   end
 
   test do
